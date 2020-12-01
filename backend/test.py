79d8226e3b27006f718a709c76a8d47e4ca50de2
Denver Massey <denmas22@live.unc.edu>
2020-12-01 18:40:05 -0500

    Added indexing variable

--- backend/test.py
+++ backend/test.py
@@ -2,16 +2,22 @@ import sys
 import time
 
 # How many arguments will each effect have?
-# brightness, beginning, ending, plus 3 extra
-num_params = 6
+# effect name, brightness, beginning, ending, plus 3 extra is 7
+num_params = 7
 
-### Get inputs for each effect ###
-#effects = float(sys.argv[::num_params])
-#params = []
+pixels = [1]*5
 
 ### Master Loop ###
 while True:
-    for effect in sys.argv[1::num_params]:
-        print(effect)
+    for i in [1:len(sys.argv):num_params]:
+        print(sys.argv[i])
+        # if (effect == "addOne"):
+            
 
     time.sleep(0.5)
+
+def addOne(pixels):
+    return [x+1 for x in pixels]
+
+def multTwo(pixels):
+    return [x*2 for x in pixels]
