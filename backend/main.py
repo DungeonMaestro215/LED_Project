3f842d1fafdb6314a0bfe5987159d0f3a202dcc8
Denver Massey <denmas22@live.unc.edu>
2020-12-01 19:34:49 -0500

    Working prototype for the main function.

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,15 @@
-### Set up NeoPixel library and initiate pixel array ###
+### Import necessary packages ### 
 import sys
 import time
 import board
 import neopixel
 import numpy as np
 
+### Import effects and create effect dictionary ###
+effect_dict = {}
+
+
+### Set up NeoPixel pixel array ###
 # Choose an open pin connected to the Data In of the NeoPixel strip
 pixel_pin = board.D12
 
@@ -20,15 +25,26 @@ pixels = neopixel.NeoPixel(
         )
 
 # How many arguments will each effect have?
-# brightness, beginning, ending, plus 3 extra
-num_params = 6
-
-### Get inputs for each effect ###
-#effects = float(sys.argv[::num_params])
-#params = []
+# effect name, brightness, beginning, ending, plus 3 extra is 7
+num_params = 7
 
 
 ### Master Loop ###
 while True:
-    for effect in sys.argv[::num_params]:
-        print(effect)
+    # Every iteration, go through each effect that was given as an argument
+    for i in range(1, len(sys.argv), num_params):
+        # Gather necessary information about the effect
+        name = sys.argv[i]              # Effect name
+        bc = float(sys.argv[i + 1])     # Brightness controller for effect (float b/t 0 and 1)
+        begin = int(sys.argv[i + 2])    # Starting index for the effect
+        end = int(sys.argv[i + 3])      # Ending index for the effct (exclusive)
+        params = sys.argv[i+4:i+num_params]      # Any additional parameters, up to 3, for the effect
+
+        # Change the appropriate part of the array by running it through the function
+        pixels = pixels[0:begin] + function_dict[name](pixels[begin:end], bc, params) + pixels[end:len(pixels)]
+
+    print(pixels)
+
+    time.sleep(0.5)
+
+
--- backend/test.py
+++ backend/test.py
@@ -1,6 +1,14 @@
 import sys
 import time
 
+# Test function
+def addOne(pixels):
+    return [x+1 for x in pixels]
+
+def multTwo(pixels):
+    return [x*2 for x in pixels]
+
+function_dict = { 'addOne':addOne, 'multTwo':multTwo }
 # How many arguments will each effect have?
 # effect name, brightness, beginning, ending, plus 3 extra is 7
 num_params = 7
@@ -10,14 +18,13 @@ pixels = [1]*5
 ### Master Loop ###
 while True:
     for i in range(1, len(sys.argv), num_params):
-        print(sys.argv[i])
-        # if (effect == "addOne"):
+        name = sys.argv[i]
+        bc = float(sys.argv[i + 1])
+        begin = int(sys.argv[i + 2])
+        end = int(sys.argv[i + 3])
+        pixels = pixels[0:begin] + function_dict[name](pixels[begin:end]) + pixels[end:len(pixels)]
+    print(pixels)
             
 
     time.sleep(0.5)
 
-def addOne(pixels):
-    return [x+1 for x in pixels]
-
-def multTwo(pixels):
-    return [x*2 for x in pixels]
