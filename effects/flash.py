cd1a0fcb792e9c708624e931e3f0bb652d425e59
Denver Massey <denmas22@live.unc.edu>
2020-12-03 19:50:51 -0500

    Fixed slight bug with flash effect

--- effects/flash.py
+++ effects/flash.py
@@ -4,7 +4,7 @@ from fill import fill
 def flash(info, bc, speed, *colors):
     # Check to see if lengths of colors is divisible by 3 
     # (every color needs r, g, and b)
-    if len(colors) % 3:
+    if len(colors) == 0 or len(colors) % 3:
         raise Exception("Flash colors argument not of the correct size.")
     # Make sure arguments are the appropriate types
     bc = float(bc)
@@ -13,4 +13,5 @@ def flash(info, bc, speed, *colors):
 
     # Which color is the current idx?
     idx = (info[1] % (len(colors) / 3)) * 3
-    return fill(info, bc, colors[idx], colors[idx+1], colors[idx+2])
+    return fill(info, bc, colors[idx], *colors[idx:idx+3])
+    # return fill(info, bc, colors[idx], colors[idx+1], colors[idx+2])
