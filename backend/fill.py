ef70fec8dfdb702acf729f2d68db2284b8b53eec
Denver Massey <denmas22@live.unc.edu>
2020-12-30 17:48:55 -0500

    hope

---
+++ backend/fill.py
@@ -0,0 +1,24 @@
+### Fills pixels with a solid color ###
+def fill(pixels, frame, bc, r, g, b):
+    # Make sure arguments are the appropriate types
+    bc = float(bc)
+    r, g, b = map(int, [r, g, b])
+    # Make sure r,g,b values are in range 0-255
+    if (r < 0):
+        r = 0
+    if (g < 0):
+        g = 0
+    if (b < 0):
+        b = 0
+    if (r > 255):
+        r = 255
+    if (g > 255):
+        g = 255
+    if (b > 255):
+        b = 255
+
+    # Put them all together and adjust for brightness
+    fill_color = [int(x*bc) for x in [r, g, b]]
+
+    # Replace each pixel with this color
+    return [fill_color]*len(pixels)
---
+++ backend/rng.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+### Randomly assign a color to every LED ###
+# change_prob - probability that any given light will change on the next frame
+# off_prob - probability that any given light will turn off
+# This is nice to be able to control, rather than just having one of the possible colors be 'off'
+def rng(pixels, frame, bc, change_prob, off_prob):
+    # Validate types for inputs
+    # bc, change_prob, off_prob = [float(x for x in [bc, change_prob, off_prob])]
+    num_pixels = len(pixels)
+    bc = float(bc)
+    change_prob = float(change_prob)
+    off_prob = float(off_prob)
+    # Loop through each pixel
+    pixels = [[0, 0, 0]] * num_pixels
+    for i in range(0, num_pixels):
+        # Turn this pixel off?
+        if (np.random.rand(1)[0] <= off_prob):
+            pixels[i] = [0, 0, 0]
+        # Change this pixel's color?
+        elif (np.random.random_sample() <= change_prob):
+            # Choose random color and scale by brightness
+            colors = np.random.rand(3)
+            pixels[i] = [int(bc * 255 * c) for c in colors]
+
+    return pixels
